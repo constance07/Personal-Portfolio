d48e8616583511e849d05ad9c760a82fe233f71c
Serenity Buckner <Serenc0107@gmail.com>
2025-10-28 12:21:08 -0400

    Day 10: Display Project Element

--- personal-portfolio/app/components/work.tsx
+++ personal-portfolio/app/components/work.tsx
@@ -1,3 +1,8 @@
+/*Functionality Imports */
+import {useState} from 'react'
+import { Link } from 'react-router-dom';
+import Image from 'next/image';
+import { useEffect } from 'react';
 /*Styled Imports*/
 import '../globals.css';
 import { Pixelify_Sans } from 'next/font/google';
@@ -7,7 +12,9 @@ import { Be_Vietnam_Pro} from 'next/font/google';
 {/*VBold*/}const be_Vietnam_Pro = Be_Vietnam_Pro({subsets: ['latin'], weight: '800'})
 {/*Light*/}const be_Vietnam_Pro_Light = Be_Vietnam_Pro({subsets: ['latin'], weight: '300'})
 {/*VLight*/}const be_Vietnam_Pro_VLight = Be_Vietnam_Pro({subsets: ['latin'], weight: '200'})
+import WGTT from '@/app/images/WGTT.jpg';
 export default function Work(){
+    
     return(
         <>
             <div className="workContainer">
@@ -16,9 +23,18 @@ export default function Work(){
                 <div id = 'h1Tags' className={pixelify_Sans_Bold.className}>&lt;/h1&gt;</div></h1> 
                 
                 <div className='displayContainer'>
-                    <div className='displayBackground'></div>
+                    <div className='displayBackground'>
                     <div id="displayTitle" className={be_Vietnam_Pro.className}>We've Got To Talk</div>
+                    <div id="displayDisc" className={be_Vietnam_Pro_Light.className}>A card game promoting self-love beyond external labels and obligation through a cycle think, act, and action prompts. </div>
+                    <Image
+                        src = {WGTT}
+                        alt = 'Weve Got To Talk Image'
+                        id = 'WGTT'
+                        quality={40}
+                        ></Image>
+                    </div>
                 </div>
+                <div id="resumeTitle" className={be_Vietnam_Pro_Light.className}>Want to know about the other cool things I do?<div id = "resumeLinkA" className={pixelify_Sans.className}> Check out my resume <a href = "https://docs.google.com/document/d/1joId81v81lhTnqoQxQcvKa-fOUb8AgJDw6EbUtR1-xs/edit?usp=sharing" id = "resumeLinkB" className={pixelify_Sans.className}>here</a></div>.</div>
             </div>
         </>
     )
